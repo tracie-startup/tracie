b7381abca0873faa60d4170e487b9398e111272b
UCD+ | René Meye <rene.meye@ucdplus.com>
2014-11-20 13:59:45 +0100

    Main Styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def body_class(body_class)
+    content_for(:body_class){body_class}
+  end
+  def page_title(page_title)
+    content_for(:page_title){page_title}
+  end
 end

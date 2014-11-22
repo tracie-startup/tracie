970b571ab838303227f1c57c65689119374247e5
UCD+ | René Meye <rene.meye@ucdplus.com>
2014-11-22 18:17:35 +0100

    Added and styled impressum

--- app/controllers/landingpage_controller.rb
+++ app/controllers/landingpage_controller.rb
@@ -2,4 +2,8 @@ class LandingpageController < ApplicationController
   def index
     @beta_request = BetaRequest.new
   end
+
+  def impressum
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'landingpage#index'
+  get 'impressum' => 'landingpage#impressum'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

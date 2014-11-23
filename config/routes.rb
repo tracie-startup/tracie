5c98cb51ece24b9c3f7f5d396921652aaf779df4
UCD+ | René Meye <rene.meye@ucdplus.com>
2014-11-23 12:56:34 +0100

    Added Datenschutz to routes and footer

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'landingpage#index'
   get 'impressum' => 'landingpage#impressum'
+  get 'datenschutz' => 'landingpage#datenschutz'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'

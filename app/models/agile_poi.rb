256286726c07fa07afd205cd75f9bcc1184734c5
UCD+ | René Meye <rene.meye@ucdplus.com>
2014-12-02 09:40:16 +0100

    Better curve

--- app/models/agile_poi.rb
+++ app/models/agile_poi.rb
@@ -1,6 +1,9 @@
 class AgilePoi < ActiveRecord::Base
   def age
     t = (Time.now - self.created_at) / 3600
-    (1 / (t + 0.01)) / 100
+    # 1/((x*x*x)/900+0.01)/100 # -> Deutlich weniger flach
+    1/((t*t*t)/100+0.01) / 100 # -> Better curve
+    #(1 / (t + 0.01)) / 100 # -> original 1/x
+    # Try yourself on: http://www.mathe-fa.de/en#result
   end
 end

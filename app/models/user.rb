deb07f20c75dae361df17a557040334ca401009d
UCD+ | René Meye <rene.meye@ucdplus.com>
2014-12-05 00:29:06 +0100

    Unregisterable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, #:registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   before_save :ensure_authentication_token

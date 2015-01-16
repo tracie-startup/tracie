505c4f51cf9921d79241604608bc32bf7b1b3983
UCD+ | René Meye <rene.meye@ucdplus.com>
2015-01-16 20:09:14 +0100

    uups too fast

--- app/models/agile_poi.rb
+++ app/models/agile_poi.rb
@@ -5,7 +5,8 @@ class AgilePoi < ActiveRecord::Base
     t = (Time.now - self.created_at) / 3600
     # 1/((x*x*x)/900+0.01)/100 # -> Deutlich flacher
     # 1/((t*t*t)/100+0.01) / 100 # -> Better curve
-    (1 / (t + 0.01)) / 100 # -> original 1/x
+    1/((t*t)/100+0.01) / 100 # -> Better curve
+    # (1 / (t + 0.01)) / 100 # -> original 1/x
     # Try yourself on: http://www.mathe-fa.de/en#result
   end
 end
